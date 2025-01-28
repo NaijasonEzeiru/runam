d5e5e182222fccfbc752a96baea11515889fd612
NaijasonEzeiru <jayseehe1035@gmail.com>
2025-01-28 21:15:22 +0100

    Major redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/*.{html,js}'],
+  content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        Inter: ["inter", "sans-serif"],
+        Righteous: ["righteous", "roboto"],
+        Slackey: ["Slackey", "sans-serif"],
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
